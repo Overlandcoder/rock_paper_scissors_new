d4736d0f2ba9d159e4a42344b4126619ad408cdf
Overlander <anuragnandal@hotmail.com>
2022-11-16 22:06:43 -0500

    Finish adding most UI features

--- script.js
+++ script.js
@@ -3,32 +3,52 @@ let getComputerChoice = () => {
   return choices[Math.floor(Math.random()*choices.length)];
 }
 
-let playRound = (playerSelection, computerSelection) => {
-  playerSelection = playerSelection.toLowerCase();
+let playerScore = 0;
+let computerScore = 0;
+let updateScore = () => score.textContent = `Player: ${playerScore} | Computer: ${computerScore}`;
+let roundOver = (playerScore >= 5 || computerScore >= 5);
+
+let playRound = (playerSelection) => {
+  computerSelection = getComputerChoice();
 
   if (playerSelection === computerSelection) {
-    return `Tie - ${playerSelection} vs ${computerSelection}`;
-  } else if (playerSelection === "rock" && computerSelection === "scissors") {
-    return `Player wins - ${playerSelection} beats ${computerSelection}`;
-  } else if (playerSelection === "rock" && computerSelection === "paper") {
-    return `Computer wins - ${computerSelection} beats ${playerSelection}`;
-  } else if (playerSelection === "paper" && computerSelection === "scissors") {
-    return `Computer wins - ${computerSelection} beats ${playerSelection}`;
-  } else if (playerSelection === "paper" && computerSelection === "rock") {
-    return `Player wins - ${playerSelection} beats ${computerSelection}`;
-  } else if (playerSelection === "scissors" && computerSelection === "paper") {
-    return `Player wins - ${playerSelection} beats ${computerSelection}`;
-  } else if (playerSelection === "scissors" && computerSelection === "rock") {
-    return `Computer wins - ${computerSelection} beats ${playerSelection}`;
+    roundWinner.textContent = `Tie - ${playerSelection} vs ${computerSelection}`;
+  } else if ((playerSelection === "rock" && computerSelection === "scissors") ||
+            (playerSelection === "scissors" && computerSelection === "paper") ||
+            (playerSelection === "paper" && computerSelection === "rock")) {
+    playerScore += 1;
+    roundWinner.textContent = `Player wins - ${playerSelection} beats ${computerSelection}`;
+  } else {
+    computerScore += 1;
+    roundWinner.textContent = `Computer wins - ${computerSelection} beats ${playerSelection}`;
+  }
+  updateScore();
+  if (roundOver) {
+    disablePlayButtons();
+    declareWinner();
   }
 }
 
-let game = () => {
-  for(i = 1; i <= 5; i++) {
-    let playerSelection = prompt("Rock paper scissors?", "");
-    let computerSelection = getComputerChoice();
-    console.log(playRound(playerSelection, computerSelection));
+function declareWinner() {
+  if (playerScore === 5) {
+    gameWinner.textContent = "Player wins the game!";
+  } else if (computerScore === 5) {
+    gameWinner.textContent = "Computer wins the game!";
   }
 }
 
-game();
+function disablePlayButtons() {
+  playButtons.forEach(btn => btn.removeEventListener());
+}
+
+const playButton = document.querySelector('.play');
+const playButtons = document.querySelectorAll('.player-selection');
+const roundWinner = document.querySelector('.round-winner');
+const gameWinner = document.querySelector('.game-winner');
+const score = document.querySelector('.score');
+// playButton.addEventListener('click', game());
+
+playButtons.forEach(btn => btn.addEventListener('click', () => {
+  let playerSelection = btn.textContent.toLowerCase();
+  playRound(playerSelection);
+}))
